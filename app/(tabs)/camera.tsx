81bfb6c4fac76dc10b948f65823b5a7a3f8f977e
gmtmaster <adamlekrinszki@gmail.com>
2025-10-04 00:35:47 +0200

    push

--- app/(tabs)/camera.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function CameraScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Kamera képernyő</Text>
-        </View>
-    );
-}
